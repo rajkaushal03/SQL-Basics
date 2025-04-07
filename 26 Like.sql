3c94a56b77cf48d69649d5e36ba130f507b13b4a
Swayam Kaushal <101446225+rajkaushal03@users.noreply.github.com>
2025-04-07 18:40:26 +0530

    first commit

---
+++ 26 Like.sql
@@ -0,0 +1,30 @@
+SELECT *
+FROM your_table
+WHERE condition LIKE 'prefix%';
+
+-- eg:-
+
+
+-- Patients table:
+-- +------------+--------------+--------------+
+-- | patient_id | patient_name | conditions   |
+-- +------------+--------------+--------------+
+-- | 1          | Daniel       | YFEV COUGH   |
+-- | 2          | Alice        |              |
+-- | 3          | Bob          | DIAB100 MYOP |
+-- | 4          | George       | ACNE DIAB100 |
+-- | 5          | Alain        | DIAB201      |
+-- +------------+--------------+--------------+
+
+/* Write your PL/SQL query statement below */
+
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%'
+
+-- +------------+--------------+--------------+
+-- | patient_id | patient_name | conditions   |
+-- +------------+--------------+--------------+
+-- | 3          | Bob          | DIAB100 MYOP |
+-- | 4          | George       | ACNE DIAB100 | 
+-- +------------+--------------+--------------+
